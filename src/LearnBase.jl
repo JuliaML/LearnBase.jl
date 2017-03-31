137920d942f877a2ff6588f356c45acb21691195
Christof Stocker <stocker.christof@gmail.com>
2017-04-01 00:42:28 +0200

    remove docstring from datasubset and replace with comments

--- src/LearnBase.jl
+++ src/LearnBase.jl
@@ -192,6 +192,10 @@ function gettarget end # not exported
 # remote data-source where the labels are part of some metadata
 # that is locally available.
 #
+# If it makes sense for the type of `data`, `obsdim` can be used
+# to disptach on which dimension of `data` denotes the observations.
+# See `?ObsDim`
+#
 # This function is implemented in MLDataPattern
 function gettargets end # not exported
 
@@ -297,24 +301,18 @@ see `MLDataPattern.RandomBatches`
 
 # --------------------------------------------------------------------
 
-"""
-    datasubset(data, [indices], [obsdim])
-
-Returns a lazy subset of the observations in `data` that correspond
-to the given `indices`. No data will be copied except of the indices.
-It is similar to calling `DataSubset(data, [indices], [obsdim])`,
-but returns a `SubArray` if the type of `data` is `Array` or `SubArray`.
-Furthermore, this function may be extended for custom types of `data`
-that also want to provide their own subset-type.
-
-If instead you want to get the subset of observations corresponding
-to the given `indices` in their native type, use `getobs`.
-
-The optional (keyword) parameter `obsdim` allows one to specify which
-dimension denotes the observations. see `ObsDim` for more detail.
-
-see `DataSubset` for more information.
-"""
+#    datasubset(data, [idx], [obsdim])
+#
+# Return a lazy subset of the observations in `data` that correspond
+# to the given `idx`. No data should be copied except of the
+# indices. Note that `idx` can be of type `Int` or `AbstractVector`.
+# Both options must be supported by a custom type.
+#
+# If it makes sense for the type of `data`, `obsdim` can be used
+# to disptach on which dimension of `data` denotes the observations.
+# See `?ObsDim`
+#
+# This function is implemented in MLDataPattern
 function datasubset end
 
 """
