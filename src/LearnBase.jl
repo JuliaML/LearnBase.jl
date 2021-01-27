289d2a968e606e833dcf963ec4ad223a4461840f
Kyle Daruwalla <daruwalla@wisc.edu>
2021-01-27 13:01:19 -0600

    Remove label encoding exports

--- src/LearnBase.jl
+++ src/LearnBase.jl
@@ -8,7 +8,6 @@ export default_obsdim, getobs, getobs!
 include("observation.jl")
 
 # LABEL ENCODING
-export nlabel, label, labeltype, poslabel, neglabel
 include("labels.jl")
 
 # LEARNING COSTS (e.g. loss & penalty)
