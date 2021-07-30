1c2bb2217072d0865617695fe9ab124899866304
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-07-30 13:36:00 +0200

    version check and more tests

--- test/observation.jl
+++ test/observation.jl
@@ -23,28 +23,57 @@ using LearnBase: getobs, nobs, default_obsdim
         X, Y = rand(2, 3), rand(3)
         dataset = (X, Y) 
         @test nobs(dataset) == 3
-        o = @inferred getobs(dataset, 2) # -> (X[:,2], Y[2])
+        if VERSION >= v"1.6"
+            o = @inferred getobs(dataset, 2)
+        else
+            o = getobs(dataset, 2)
+        end
         @test o[1] == X[:,2]
         @test o[2] == Y[2]
 
-        o = @inferred getobs(dataset, 1:2) # -> (X[:,1:2], Y[1:2])
+        if VERSION >= v"1.6"
+            o = @inferred getobs(dataset, 1:2)
+        else
+            o = getobs(dataset, 2)
+        end
+        
         @test o[1] == X[:,1:2]
         @test o[2] == Y[1:2]
+    end
 
-        o = @inferred getobs(dataset, 1:2)
+
+    @testset "named tuple" begin
+        X, Y = rand(2, 3), rand(3)
+        dataset = (x=X, y=Y) 
+        @test nobs(dataset) == 3
+        if VERSION >= v"1.6"
+            o = @inferred getobs(dataset, 2)
+        else
+            o = getobs(dataset, 2)
+        end
+        @test o.x == X[:,2]
+        @test o.y == Y[2]
+        
+        if VERSION >= v"1.6"
+            o = @inferred getobs(dataset, 1:2)
+        else
+            o = getobs(dataset, 2)
+        end
+        @test o.x == X[:,1:2]
+        @test o.y == Y[1:2]
     end
 
     @testset "dict" begin
-        # A dataset with 3 observations, each with 2 input features
         X, Y = rand(2, 3), rand(3)
         dataset = Dict("X" => X, "Y" => Y) 
         @test nobs(dataset) == 3
+        
         # o = @inferred getobs(dataset, 2) # not inferred
         o = getobs(dataset, 2)
         @test o["X"] == X[:,2]
         @test o["Y"] == Y[2]
 
-        o = getobs(dataset, 1:2) # -> (X[:,1:2], Y[1:2])
+        o = getobs(dataset, 1:2)
         @test o["X"] == X[:,1:2]
         @test o["Y"] == Y[1:2]
     end
